d0bfab4792b28357a61e35f4cc9db037db593d7d
Matgenius04 <Matgenius04@users.noreply.github.com>
2018-02-16 20:34:24 -0500

    added more  changes

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 let users = JSON.parse(localStorage.getItem("users"));
 
-function storeKey() {
-  let keyname = document.getElementById('name');
-  users.push(keyname)={
-  key : document.getElementById('key')
-}
-  localStorage.setItem("users", JSON.stringify(users));
-}
+//function activateKey() {
+  //if () {
+    function storeKey() {
+      users.push(document.getElementById('name'))={
+      key : document.getElementById('key')
+    };
+      localStorage.setItem("users", JSON.stringify(users));
+      let users = JSON.parse(localStorage.getItem("users"));
+      console.log(users)
+    }
+  //} else {
+
+  }
+//}
